a1b558561bab08f1169ddf0162b45bb006200ac4
Alice <aliceroherty@gmail.com>
2023-05-07 17:00:26 -0300

    Enable cors

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
@@ -8,6 +9,14 @@ mongoose.connect(process.env.CONNECTION_STRING);
 
 const db = mongoose.connection;
 db.on('error', (error) => console.error(error));
+
+app.use(
+	cors({
+		origin: '*',
+		credentials: true,
+		optionSuccessStatus: 200,
+	})
+);
 
 app.use(express.json());
 
