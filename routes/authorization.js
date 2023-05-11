1fb589be46b85659a0f3c8eb46ae466a758d0ef0
Alice <aliceroherty@gmail.com>
2023-05-11 18:52:48 -0300

    Create login route
    Relates to #1

--- routes/authorization.js
+++ routes/authorization.js
@@ -3,4 +3,17 @@ const jwt = require('jsonwebtoken');
 
 const router = express.Router();
 
+router.post('/login', (req, res) => {
+	try {
+		const apiKey = req.body.apiKey;
+		const accessToken = jwt.sign(
+			{ apiKey },
+			process.env.ACCESS_TOKEN_SECRET
+		);
+		res.json({ accessToken });
+	} catch (error) {
+		res.status(500).json({ message: error.message });
+	}
+});
+
 module.exports = router;
