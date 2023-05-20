fec240759cedc03b02f1b7d8e10fc1358e8075c0
Alice <aliceroherty@gmail.com>
2023-05-20 12:27:16 -0300

    Update server to run on port specified in .env

--- app.js
+++ app.js
@@ -28,4 +28,6 @@ app.use('/auth', authorizationRouter);
 
 app.use('/images', express.static('images'));
 
-app.listen(3000, () => console.log('Server listening on port 3000...'));
+app.listen(process.env.PORT, () =>
+	console.log(`Server listening on port ${process.env.PORT}...`)
+);
